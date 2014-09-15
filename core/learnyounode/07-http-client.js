cfb5ce9f1fccb4a3e735f0604ed3a53fb758a175
Jasper <jl@eyeconic.de>
2014-09-15 20:48:05 +0200

    added exercise 7

---
+++ core/learnyounode/07-http-client.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var url = process.argv[2];
+http.get(url, function(res) {
+  res.setEncoding('utf8');
+  res.on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
+
+  res.on('data', function(data) {
+    console.log(data);
+  });
+
+});
+
